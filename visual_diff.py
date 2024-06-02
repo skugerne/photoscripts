8c4b189904c2d4026b6935c768230ee9accc4556
skugerne <ray.skoog@gmail.com>
2024-06-03 01:06:46 +0200

    Display outlines where images will go.

--- visual_diff.py
+++ visual_diff.py
@@ -27,20 +27,107 @@ args = None
 
 
 
+def build_date_checksum_list(image_info_list):
+    """
+    Convert a list of tuples (date,checksum) to a dict of checksums pointing to indexes.
+    """
+
+    image_list = set()
+    for image_info in image_info_list:
+        image_list.add((image_info.date,image_info.checksum))
+    image_list = sorted(image_list)
+
+    checksum_to_idx = defaultdict(lambda: set())
+    for idx,vals in enumerate(image_list):
+        checksum_to_idx[vals[1]].add(idx)
+    checksum_to_idx2 = dict()
+    for k in checksum_to_idx.keys():
+        checksum_to_idx2[k] = sorted(checksum_to_idx[k])
+    return image_list, checksum_to_idx2
+
+
+
+def missing_image(dims):
+    """
+    Return a surface of the given dimentions which indicates a missing image.
+    """
+
+    surf = pygame.Surface(dims)
+    pygame.draw.line(surf, (255,0,0), (0,0), dims)
+    pygame.draw.line(surf, (0,0,255), (dims[0],0), (0,dims[1]))
+    txt_srf = text_box("missing", (150,150,220), (0,0,0), size=16)
+    surf.blit(txt_srf,((dims[0]-txt_srf.get_width())/2,(dims[1]-txt_srf.get_height())/2))
+    return surf
+
+
+
 class ImageRow():
-    def __init__(self, image_info_list, all_images, checksum_to_idx, screen_res, screen_srf, upper_left):
+    def __init__(self, image_info_list, all_images, checksum_to_idx, screen_srf, upper_left, dims):
         self.image_info_list = image_info_list       # InventoryItem objects for our inventory file, theoretically sorted oldest first
+        self.own_images, self.own_checksum_to_idx = build_date_checksum_list(image_info_list)
         self.all_images = all_images                 # sorted tuples (date,checksum) for both inventory files, sorted oldest first
         self.all_checksum_to_idx = checksum_to_idx   # checksums pointing to indexes in 'all_images'
         self.idx = 0
-        self.cache = ImageCache(screen_res, [x.name for x in image_info_list])
         self.upper_left = upper_left
+        self.dims = dims
 
-    def show(self, checksum):
+        self.screen_srf = screen_srf
+        self.num_surfaces = 7
+        self.surfaces = [None]*self.num_surfaces
+
+        self.main_dims = (dims[0]/3, dims[1])
+        self.small_dims = (dims[0]/9, 3*dims[1]/4)
+        logger.info("Main surface dims: %s" % str(self.main_dims))
+        logger.info("Small surface dims: %s" % str(self.small_dims))
+        self.surface_corn = [None]*self.num_surfaces
+        x = upper_left[0]
+        for idx in range(self.num_surfaces):
+            if idx == 3:
+                self.surface_corn[idx] = (x,self.upper_left[1])
+                x += self.main_dims[0]
+            else:
+                offset = (self.main_dims[1] - self.small_dims[1]) / 2
+                self.surface_corn[idx] = (x,self.upper_left[1]+offset)
+                x += self.small_dims[0]
+        logger.info("Surface corners: %s" % str(self.surface_corn))
+        self.main_missing = missing_image(self.main_dims)
+        self.small_missing = missing_image(self.small_dims)
+
+        self.cache = ImageCache(self.main_dims, [x.name for x in image_info_list], args.cache_count)
+
+    def set_idx(self, idx):
         """
         Show the image with the given checksum, or a placeholder if it is missing.
         """
-        pass
+        for idx in range(len(self.surfaces)):
+            pass
+
+    def display(self):
+        """
+        Blit images to screen.
+        """
+
+        for idx in range(self.num_surfaces):
+            logger.info("Draw surface %d." % idx)
+            corn = self.surface_corn[idx]
+            if idx == 3:
+                dims = self.main_dims
+            else:
+                dims = self.small_dims
+            if self.surfaces[idx]:
+                srf = self.surfaces[idx]
+            elif idx == 3:
+                srf = self.main_missing
+            else:
+                srf = self.small_missing
+            self.screen_srf.blit(srf,corn)
+            p = (
+                corn,
+                (corn[0],corn[1]+dims[1]),
+                (corn[0]+dims[0],corn[1]+dims[1]),
+                (corn[0]+dims[0],corn[1])
+            )
+            pygame.draw.lines(self.screen_srf, (120,120,120), True, p)
 
 
 
@@ -51,43 +138,25 @@ def start_show(image_info_list_1, image_info_list_2):
     pygame.display.init()
     screen_res, screen_srf = apply_screen_setting(False)
 
-    all_images = set()
-    for image_info in image_info_list_1 + image_info_list_2:
-        all_images.add((image_info.date,image_info.checksum))
-    all_images = sorted(all_images)
-    checksum_to_idx = dict((vals[1],idx) for idx,vals in enumerate(all_images))
-    idx = 0
+    all_images, checksum_to_idx = build_date_checksum_list(image_info_list_1 + image_info_list_2)
 
-    upper_row = ImageRow(image_info_list_1, all_images, checksum_to_idx, screen_res, screen_srf, (0,0))
-    lower_row = ImageRow(image_info_list_2, all_images, checksum_to_idx, screen_res, screen_srf, (0,screen_res[1]/2))
-    upper_row.show(all_images[0][1])
-    lower_row.show(all_images[0][1])
+    row_dims = (screen_res[0],screen_res[1]/2)
+    upper_row = ImageRow(image_info_list_1, all_images, checksum_to_idx, screen_srf, (0,0), row_dims)
+    lower_row = ImageRow(image_info_list_2, all_images, checksum_to_idx, screen_srf, (0,screen_res[1]/2), row_dims)
 
     fullscreen = False
     stop = False
     new_idx_chosen = True
     idx = 0
-    start_at = 0
-    direction = 1
-    flip_time = 2
     while not stop:
-        if new_idx_chosen or time() - start_at > flip_time:
-            if not new_idx_chosen:
-                idx += direction
-                logger.debug("Increment/decrement image index to %d." % idx)
-                new_idx_chosen = True
-            srf = cache.get_surface(idx)
-            if srf:
-                logger.info("Show image #%d of %d." % (idx+1,len(paths)))
-                screen_srf.blit(srf,(0,0))
-                txt_srf = text_box("%04d-%02d-%02d" % tuple(path_to_date[paths[idx]][0:3]), (255,255,255), (0,0,0))
-                screen_srf.blit(txt_srf,(0,0))
-                pygame.display.flip()
-                start_at = time()
-                new_idx_chosen = False
-            else:
-                logger.debug("Image #%d of %d not available yet." % (idx+1,len(paths)))
+        if new_idx_chosen:
+            upper_row.set_idx(idx)
+            lower_row.set_idx(idx)
+            new_idx_chosen = False
         else:
+            upper_row.display()
+            lower_row.display()
+            pygame.display.flip()
             sleep(0.05)
 
         for event in pygame.event.get():
@@ -105,11 +174,12 @@ def start_show(image_info_list_1, image_info_list_2):
                 elif event.key == pygame.K_f:       # toggle fullscreen
                     fullscreen = not fullscreen
                     screen_res, screen_srf = apply_screen_setting(fullscreen)
-                    upper_row = ImageRow(image_info_list_1, all_images, checksum_to_idx, screen_res, screen_srf, (0,0))
-                    lower_row = ImageRow(image_info_list_2, all_images, checksum_to_idx, screen_res, screen_srf, (0,screen_res[1]/2))
+                    row_dims = (screen_res[0],screen_res[1]/2)
+                    upper_row = ImageRow(image_info_list_1, all_images, checksum_to_idx, screen_srf, (0,0), row_dims)
+                    lower_row = ImageRow(image_info_list_2, all_images, checksum_to_idx, screen_srf, (0,screen_res[1]/2), row_dims)
 
         if idx < 0: idx = 0
-        if idx >= len(paths): idx = len(paths)-1
+        if idx >= len(all_images): idx = len(all_images)-1
 
     pygame.quit()
 
